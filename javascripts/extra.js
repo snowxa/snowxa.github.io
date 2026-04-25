84134b3a546b5466bc8410f6bcdf7ae888590831
snow <360244014@qq.com>
2026-04-25 15:27:07 +0800

    提交博客

---
+++ javascripts/extra.js
@@ -0,0 +1,10 @@
+/* ==========================================
+   Zensical-Chinese-Tutorial 自定义脚本
+   ========================================== */
+
+// 即时导航兼容
+document$.subscribe(function() {
+  console.log('Zensical loaded');
+  
+  // 在这里添加你的自定义 JavaScript
+});
